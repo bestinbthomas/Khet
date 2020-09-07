173ac6f0182fce825d358c0c90cbe0b2314a8a6e
Sudhindra Dondapati <sudhindra5678@gmail.com>
2020-09-07 20:21:18 +0530

    added necessary enum classes for piece class

---
+++ app/src/main/java/org/dwoc/khet/models/enums/Player.kt
@@ -0,0 +1,6 @@
+package org.dwoc.khet.models.enums
+
+enum class Player(val i: Int) {
+  PLAYER1(1),
+  PLAYER2(-1)
+}
---
+++ app/src/main/java/org/dwoc/khet/models/enums/Type.kt
@@ -0,0 +1,10 @@
+package org.dwoc.khet.models.enums
+
+enum class Type {
+  PHARAOH,
+  SCARAB,
+  PYRAMID,
+  OBELISK,
+  ANUBIS,
+  SPHINX
+}
