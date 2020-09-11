02c7634bf2372c709e4aeab87b705acc74c7d469
Varunalingam <varunalingam.v@gmail.com>
2020-09-11 17:34:11 +0530

    docs<Piece> : javadoc comments
    Converted Commenting Style to javadoc Commenting(Piece.kt)

--- app/src/main/java/org/dwoc/khet/gameControl/Piece.kt
+++ app/src/main/java/org/dwoc/khet/gameControl/Piece.kt
@@ -3,18 +3,27 @@ package org.dwoc.khet.gameControl
 import org.dwoc.khet.models.enums.Direction
 import org.dwoc.khet.models.enums.Player
 
-// Each Types of Piece will Extend this Piece Class
+/**
+ * Each Types of Piece will Extend this Piece Class
+ */
 abstract class Piece(val PlayerId : Player, var PosX : Int, var PosY : Int, var Orientation : Direction)
 {
 
-  //Function MovePiece will set the PosX and PosY of the Piece
+  /**
+   * MovePiece will set the PosX and PosY of the Piece
+   * @param PosX The X-Coordinate of the Position to move
+   * @param PosY The Y-Coordinate of the Position to move
+   */
   fun MovePiece (PosX : Int,PosY : Int)
   {
     this.PosX = PosX
     this.PosY = PosY
   }
 
-  //Function RotatePiece will Rotate the Piece's Orientation by the Said Amount
+  /**
+   * RotatePiece will Rotate the Piece's Orientation by the Said amount
+   * @param amount the amount to rotate will take values between -3 to 3
+   */
   fun RotatePiece (amount : Int)
   {
      var value: Int = amount + Orientation.ordinal + 4
@@ -22,9 +31,12 @@ abstract class Piece(val PlayerId : Player, var PosX : Int, var PosY : Int, var 
     Orientation = Direction.values()[value]
   }
 
-  //Function attackFrom will use the rayDirection to calculate whether or not the object hits
-  // and if reflects the output ray's direction
-  // and Returns a Pair of Boolean (Hit) and Direction (null if the ray is hit)
+  /**
+   * attackFrom will use the rayDirection to calculate whether or not the object hits
+   * and if reflects the output ray's direction
+   * @param rayDirection the Direction of the Incident ray with respect to the Board
+   * @return a Pair of Boolean (Hit on Object) and Direction (null if the ray is hit)
+   */
   abstract fun attackFrom(rayDirection : Direction) : Pair<Boolean , Direction?>
 
 }
