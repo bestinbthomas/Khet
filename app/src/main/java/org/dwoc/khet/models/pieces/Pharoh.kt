26dc08bb0b03d9edadbf63db286be5197bcdb218
Varunalingam <varunalingam.v@gmail.com>
2020-10-06 02:06:47 +0530

    refactor : compatability with abstract class Piece
    update Scarab class (Scarab.kt)
    
    update Pharoh class (Pharoh.kt)

--- app/src/main/java/org/dwoc/khet/models/pieces/Pharoh.kt
+++ app/src/main/java/org/dwoc/khet/models/pieces/Pharoh.kt
@@ -1,15 +1,20 @@
 package org.dwoc.khet.models.pieces
 
+import org.dwoc.khet.models.Position
 import org.dwoc.khet.models.enums.Direction
 import org.dwoc.khet.models.enums.Player
+import org.dwoc.khet.models.enums.Type
 
 /**
  * Pharoh Class Extends Piece Class
  * This Class is used to store Pharoh Pieces
  */
-class Pharoh(PlayerId : Player, PosX : Int, PosY : Int, Orientation : Direction)  : Piece(PlayerId, PosX, PosY, Orientation)  {
+class Pharoh(PlayerId : Player, Position: Position, Orientation : Direction)  : Piece(Type.PHARAOH, PlayerId, Position, Orientation)  {
 
   override fun attackFrom(rayDirection: Direction): Pair<Boolean, Direction?> {
      return Pair(true,null)
   }
+
+  override val canReplace: Boolean
+    get() = false
 }
--- app/src/main/java/org/dwoc/khet/models/pieces/Scarab.kt
+++ app/src/main/java/org/dwoc/khet/models/pieces/Scarab.kt
@@ -1,13 +1,15 @@
 package org.dwoc.khet.models.pieces
 
+import org.dwoc.khet.models.Position
 import org.dwoc.khet.models.enums.Direction
 import org.dwoc.khet.models.enums.Player
+import org.dwoc.khet.models.enums.Type
 
 /**
  * Scarab Class Extends Piece Class
  * This Class is used to store Scarab or Djed Pieces
  */
-class Scarab(PlayerId : Player, PosX : Int, PosY : Int, Orientation : Direction)  : Piece(PlayerId, PosX, PosY, Orientation)  {
+class Scarab(PlayerId : Player, Position : Position, Orientation : Direction)  : Piece(Type.SCARAB, PlayerId, Position, Orientation)  {
 
   override fun attackFrom(rayDirection: Direction): Pair<Boolean, Direction?> {
 
@@ -26,4 +28,7 @@ class Scarab(PlayerId : Player, PosX : Int, PosY : Int, Orientation : Direction)
 
     return Pair(false, Direction.values()[dir])
   }
+
+  override val canReplace: Boolean
+    get() = true
 }
